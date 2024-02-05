15481303ebd7c72db572a1b1a1318063e13e17fe
Daniel Souza <149512463+dev-danielsouza@users.noreply.github.com>
2024-02-05 15:25:32 -0300

    bugfix: d-01

--- desafios/d-01/script.js
+++ desafios/d-01/script.js
@@ -4,18 +4,20 @@ var mss = window.document.querySelector("div#dayMss");
 var img = window.document.querySelector("img.main-img");
 
 if (dayHour < 12) {
-    mss.innerHTML = `<p>Bom Dia!</p>`;
+    mss.innerHTML = `<p> Agora são <strong>${dayHour} horas.</strong>.Bom Dia!</p>`;
     mss.style.color = '#C8B68B'
     img.setAttribute("src","components/images/morning.jpg")
     window.document.body.style.background = '#C8B68B'
-} else if (dayHour <= 18) {
-    mss.innerHTML = '<p>Boa Tarde!</p>'
+} else if (dayHour > 12 && dayHour <= 18) {
+    mss.innerHTML = `<p> Agora são <strong>${dayHour} horas.</strong> Boa Tarde!</p>`;
     mss.style.color = '#AB7A65'
     img.setAttribute("src","components/images/afternoon.jpg")
     window.document.body.style.background = '#AB7A65'
-} else {
-    mss.innerHTML = '<p>Boa Noite!</p>'
+} else if (dayHour > 18) {
+    mss.innerHTML = `<p> Agora são <strong>${dayHour} horas.</strong> Boa Noite!</p>`;
     mss.style.color = '#48494A'
     img.setAttribute("msg", "components/images/night.jpg")
     window.document.body.style.background = '#48494A'
+} else {
+    mss.innerHTML = '<p>[ERRO]</p>'
 }
