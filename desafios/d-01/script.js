178813e48de2a64018b69141c4ac9c1f52add689
Daniel Souza <149512463+dev-danielsouza@users.noreply.github.com>
2024-02-05 15:07:34 -0300

    d-01 finished

---
+++ desafios/d-01/script.js
@@ -0,0 +1,21 @@
+var hour = new Date();
+var dayHour = hour.getHours();
+var mss = window.document.querySelector("div#dayMss");
+var img = window.document.querySelector("img.main-img");
+
+if (dayHour < 12) {
+    mss.innerHTML = `<p>Bom Dia!</p>`;
+    mss.style.color = '#C8B68B'
+    img.setAttribute("src","components/images/morning.jpg")
+    window.document.body.style.background = '#C8B68B'
+} else if (dayHour <= 18) {
+    mss.innerHTML = '<p>Boa Tarde!</p>'
+    mss.style.color = '#AB7A65'
+    img.setAttribute("src","components/images/afternoon.jpg")
+    window.document.body.style.background = '#AB7A65'
+} else {
+    mss.innerHTML = '<p>Boa Noite!</p>'
+    mss.style.color = '#48494A'
+    img.setAttribute("msg", "components/images/night.jpg")
+    window.document.body.style.background = '#48494A'
+}
